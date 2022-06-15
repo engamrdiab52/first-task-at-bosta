a4ec32df150c0909843f79e78ab4586bbebc0272
engam <engamrdiab52@gmail.com>
2022-06-15 10:38:12 +0200

    add hilt to activity and application

---
+++ app/src/main/java/com/amrabdelhamiddiab/firsttaskatbosta/BostaApplication.kt
@@ -0,0 +1,8 @@
+package com.amrabdelhamiddiab.firsttaskatbosta
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BostaApplication: Application() {
+}
